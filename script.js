e13cd33bd5c8ee65d48f0569e8f0e4720632c8dd
moth <102203406+br3adjam@users.noreply.github.com>
2025-07-09 17:48:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let newWindow2,a,b,c;
 let i = 0;
 
+function fakeAlert(msg) {
+  document.getElementById("alertText").textContent = msg;
+  document.getElementById("fakeAlert").style.display = "block";
+}
+
+
 function openAndMonitor() {
     newWindow2 = window.open("https://br3adjam.github.io", '_blank', 'fullscreen=yes,scrollbars=auto');
     
@@ -27,6 +33,9 @@ function go() {
     i++;
     const titleText = document.getElementById("title");
     titleText.textContent = "yip" + "e".repeat(i);  
+    if(i>2){
+        fakeAlert("you clicked too much");
+    }
 
     const test2Text = document.getElementById("test2");
     test2Text.textContent = i;
